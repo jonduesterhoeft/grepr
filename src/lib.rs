d368cff33870d40abc6f8bb08d23628aa331b668
Jon Duesterhoeft <136846952+jonduesterhoeft@users.noreply.github.com>
2023-07-02 19:57:45 +0000

    fixed mut ref

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ pub struct Config {
 /// Executes the search and outputs results.
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut reader = read(&config.path)?;
-    let results = search(&config.query, &reader)?;
+    let results = search(&config.query, &mut reader)?;
 
     write(&results, &mut std::io::stdout())?;
 

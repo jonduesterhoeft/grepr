999946eaafc69ebababc36ed3b71ebe7e473b9b3
Jon Duesterhoeft <136846952+jonduesterhoeft@users.noreply.github.com>
2023-07-02 15:58:11 +0000

    fixed pub build

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub struct Config {
 
 impl Config {
     /// Parses the command line arguments into the query and file path.
-    fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
         // args[0] is taken up by the program's name
         if args.len() < 3 {
             return Err("not enough arguments");
